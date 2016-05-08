45969adabe27cfbbf7191dcec2f8b60f488482a9
Louis Phan <icoding.active@gmail.com>
2016-05-08 08:18:07 +0700

    Initiate Project

--- app/Templates/Admin/header.php
+++ app/Templates/Admin/header.php
@@ -2,88 +2,158 @@
 <html lang="<?php echo LANGUAGE_CODE; ?>">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title.' - '.SITETITLE;?></title>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <?php
     echo $meta;//place to pass data / plugable hook zone
     Assets::css([
-        Url::templateHomePath().'css/materialize.min.css',
-        Url::templateHomePath().'css/style.css',
+        Url::templatePath().'css/bootstrap.min.css',
+        Url::templatePath().'css/bootstrap-theme.min.css',
+        Url::templatePath().'font-awesome/css/font-awesome.css',
+        Url::templatePath().'css/animate.css',
+        Url::templatePath().'css/bootstrap-switch.min.css',
+        Url::templatePath().'css/dataTables.bootstrap.css',
+        Url::templatePath().'css/bootstrap-select.css',
+        Url::templatePath().'css/datepicker.css',
+        Url::templatePath().'css/style.css',
     ]);
     echo $css; //place to pass data / plugable hook zone
     ?>
 
     <?php
-        Assets::js([
-        Url::templatePath().'js/jquery-2.1.1.js',
-        Url::templatePath().'js/moment.js',
-        Url::templatePath().'js/tinymce/tinymce.min.js',
-        Url::templatePath().'js/tinymce/jquery.tinymce.min.js',
-        Url::templatePath().'js/jquery.validate.js'
-    ]);
-    ?>
+    Assets::js([
+    Url::templatePath().'js/jquery-2.1.1.js',
+    Url::templatePath().'js/bootstrap-select.js',
+    Url::templatePath().'js/bootstrap-datepicker.js',
+    Url::templatePath().'js/bootstrap-switch.min.js',
+    Url::templatePath().'js/moment.js',
+    Url::templatePath().'js/tinymce/tinymce.min.js',
+    Url::templatePath().'js/tinymce/jquery.tinymce.min.js',
+    Url::templatePath().'js/jquery.dataTables.js',
+    Url::templatePath().'js/dataTables.bootstrap.js',
+    Url::templatePath().'js/jquery.validate.js',
+    Url::templatePath().'js/bootstrap.min.js',
+]);
+?>
+<script>
+    tinymce.init({
+        selector: "textarea",
+        statusbar: false,
+        setup: function (editor) {
+            editor.on('change', function () {
+                tinymce.triggerSave();
+            });
+        }
+    });
+</script>
 </head>
 <body>
-    <nav class="orange darken-4" role="navigation">
-        <div class="nav-wrapper container">
-            <?php 
-                if(Session::get('user')[0]->username == '')
-                {
-            ?>
-            <ul class="right hide-on-med-and-down">
-                <li><a class="sign-in-modal modal-trigger" data-target="signup" >Sign In</a></li>
-                <li><a class="sign-up-modal modal-trigger" data-target="signup" >Sign Up</a></li>
-            </ul>
-            <?php 
-                }
-            ?>
-            <?php 
-                if(Session::get('user')[0]->username != '')
-                {
-            ?>
-            <ul class="right">
-                <li>
-                    <a class="dropdown-button active" data-activates="dropdown2">
-                            <img class="circle" style="width: 45px; vertical-align: middle" src="<?php echo Session::get('user')[0]->avatar ?>">
-                            <?php echo Session::get('user')[0]->fullname ?>
-                    </a>
-                    <ul id="dropdown2" class="dropdown-content active" style="white-space: nowrap; position: absolute; top: 56px; left: 1126.59px; opacity: 1; display: block;">     
-                        <li style="width: 100%"><a href="<?=DIR;?>logout">Đăng xuất</a></li>
+<?php echo $afterBody; //place to pass data / plugable hook zone?>
+<div id="wrapper">
+    <nav class="navbar-default navbar-static-side" role="navigation">
+        <div class="sidebar-collapse">
+            <ul class="nav metismenu" id="side-menu">
+                <li class="nav-header">
+                    <div class="dropdown profile-element">
+                        <span>
+                        <img alt="image" class="img-rounded" width="50px" height="50px"
+                            src="<?php echo Session::get('admin')[0]->avatar ?>" />
+                        </span> 
+                        <a data-toggle="dropdown" class="dropdown-toggle" href="#"> 
+                        <span class="clear">
+                            <span class="block m-t-xs"> <strong
+                                    class="font-bold"><?php echo Session::get('admin')[0]->fullname ?></strong>
+                            </span>
+                            <!-- <span class="departmentCur text-muted text-xs block">Phòng: Art Director</span> -->
+                        </span>
+                        </a>
+                        <ul class="dropdown-menu animated fadeInRight m-t-xs">
+                            <li><a href="<?=DIR;?>admin/profile">Profile</a></li>
+                            <li><a href="<?=DIR;?>admin/change-password">Change Password</a></li>
+                            <li class="divider"></li>
+                            <li><a href="<?=DIR;?>admin/logout">Exit</a></li>
+                        </ul>
+                    </div>
+                    <div class="logo-element">IN+</div>
+                </li>
+                <?php if(Session::get('admin')[0]->roleName == 'admin') {?>
+                <li class="user <?php if($menu == 'preference') echo 'active'; ?> "><a href="#"><i class="fa fa-th-large"></i> <span
+                    class="nav-label">General</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li><a href="<?=DIR;?>admin/dashboard">Dashboard</a></li>
                     </ul>
                 </li>
+                <li class="user <?php if($menu == 'user') echo 'active'; ?> "><a href="#"><i class="fa fa-th-large"></i> <span
+                    class="nav-label">Manage User</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li><a href="<?=DIR;?>admin/user">Manage Users</a></li>
+                        <li><a href="<?=DIR;?>admin/role">Manage Roles</a></li>
+                    </ul>
+                </li>
+                <?php } ?>
+                <?php if(Session::get('admin')[0]->roleName == 'teacher' || Session::get('admin')[0]->roleName == 'admin') {?>
+                <li class="exam <?php if($menu == 'exam') echo 'active'; ?> "><a href="#"><i class="fa fa-th-large"></i> <span
+                    class="nav-label">Manage Examination</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li><a href="<?=DIR;?>admin/level">Manage Levels</a></li>
+                        <li><a href="<?=DIR;?>admin/question-answer">Question & Answer</a></li>
+                        <!-- <li><a href="<?=DIR;?>admin/check-exams">Check Exams</a></li> -->
+                    </ul>
+                </li>
+                <?php } ?>
+                <?php if(Session::get('admin')[0]->roleName == 'admin') {?>
+                <li class="news <?php if($menu == 'news') echo 'active'; ?> "><a href="#"><i class="fa fa-th-large"></i> <span
+                    class="nav-label">News</span> <span class="fa arrow"></span></a>
+                    <ul class="nav nav-second-level collapse">
+                        <li><a href="<?=DIR;?>admin/notification">Manage Notification</a></li>
+                        <li><a href="<?=DIR;?>admin/news">Manage News</a></li>
+                    </ul>
+                </li>
+                <?php } ?>
             </ul>
-            <?php 
-                }
-            ?>
         </div>
     </nav>
-    <nav class="black" role="navigation">
-        <div class="nav-wrapper container">
-          <a id="white-text logo-container" href="<?=DIR;?>home" class="brand-logo">English Club</a>
-          <ul class="right hide-on-med-and-down">
-            <li><a href="<?=DIR;?>home">Home</a></li>
-            <?php 
-                if(Session::get('user')[0]->username != '')
-                {
-            ?>
-            <li><a href="<?=DIR;?>exam">Examination</a></li>
-            <li><a href="<?=DIR;?>history">History</a></li>
-            <?php 
-                }
-            ?>
-            <li><a href="<?=DIR;?>news">News</a></li>
-            <li><a href="<?=DIR;?>notifications">Notifications</a></li>
-            <li><a href="<?=DIR;?>about-us">About Us</a></li>
-          </ul>
+    <div id="page-wrapper" class="gray-bg">
+    <div class="row border-bottom">
+    <nav class="navbar navbar-static-top" role="navigation"
+        style="margin-bottom: 0">
+        <div class="navbar-header">
+            <a class="navbar-minimalize minimalize-styl-2 btn btn-primary "
+                href="#"><i class="fa fa-bars"></i> </a>
+            <!-- <form role="search" class="navbar-form-custom"
+                action="search_results.html">
+                <div class="form-group">
+                    <input type="text" placeholder="Tìm kiếm..."
+                        class="form-control" name="top-search" id="top-search">
+                </div>
+            </form> -->
+        </div>
+        <ul class="nav navbar-top-links navbar-right">
+            <li><span class="m-r-sm text-muted welcome-message">Hello,
+                    <strong><?php echo Session::get('admin')[0]->username ?></strong> ! </span></li>
+            <!-- <li class="dropdown"><a class="count-info"
+                href="<c:url value='/admin/viewNoti'/>"> <i class="fa fa-bell"></i> <span
+                    class="label label-primary">1</span>
+            </a>
+            </li> -->
 
-          <ul id="nav-mobile" class="side-nav">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Examination</a></li>
-            <li><a href="#">News</a></li>
-            <li><a href="#">Notifications</a></li>
-            <li><a href="#">About Us</a></li>
-          </ul>
-          <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-        </div>
+
+            <li><a href="<?=DIR;?>admin/logout"> <i
+                    class="fa fa-sign-out"></i> Log out
+            </a></li>
+        </ul>
+
     </nav>
+</div>
+<div class="row wrapper border-bottom white-bg page-heading">
+    <div class="col-lg-10">
+        <h2><?= $title ?></h2>
+        <ol class="breadcrumb">
+            <li><a href="<?=DIR;?>">Home</a></li>
+            <li class="active"><strong><?= $title ?></strong></li>
+        </ol>
+    </div>
+    <div class="col-lg-2"></div>
+</div>
+</nav>
+<div class="wrapper wrapper-content  animated fadeInRight blog">
