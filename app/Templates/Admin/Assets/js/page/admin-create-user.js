f5508ce31804b2cccf44fe330ee912df1fc579a4
Phan Tuan Tu <icoding.active@gmail.com>
2016-05-26 08:59:24 +0700

    -- Commit something new ---

--- app/Templates/Admin/Assets/js/page/admin-create-user.js
+++ app/Templates/Admin/Assets/js/page/admin-create-user.js
@@ -1,5 +1,6 @@
 $(function() {
 	StringUtil.setMessage(escapeHtml("Ghi chú : Bạn sẽ tạo được người dùng mới."),'alert-info');
+	createUserForm.validate();
 });
 
 var createUserForm = {
@@ -7,15 +8,12 @@ var createUserForm = {
 
 	},
 	validate : function(){
-		$('#createForm').validate(){
+		$('#createForm').validate({
 			rules : {
 				username :{
 					required :true,
-					minlength : 6,
-					remote : {
-
-					}
-				}
+					minlength : 6
+				},
 				password : {
 					required : true,
 					minlength : 6
@@ -23,12 +21,40 @@ var createUserForm = {
 				confirmPassword : {
 					required : true,
 					equalTo : '.password'
+				},
+				email : {
+					required : true,
+					email : true
+				},
+				fullName : {
+					required : true
 				}
 			},
 			messages: {
-
+				username : {
+					required : 'Không được để trống',
+					minlength : 'Độ dài tối thiểu là 6',
+				},
+				password : {
+					required : 'Không được để trống',
+					minlength : 'Độ dài tối thiểu là 6'
+				},
+				confirmPassword : {
+					required : 'Không được để trống',
+					equalTo : 'Mật khẩu và xác nhận mật khẩu không giống nhau'
+				},
+				email : {
+					required : 'Không được để trống',
+					email : 'Không đúng định dạng email'
+				},
+				fullName : {
+					required : 'Không được để trống'
+				}
 			}
-		}
+		});
+	},
+	submit : function(){
+		alert('Dep trai');
 	}
 }
 
--- app/Views/Admin/User/CreateUser.php
+++ app/Views/Admin/User/CreateUser.php
@@ -17,7 +17,7 @@
           <div class="form-group">
             <label for="password" class="col-sm-2 control-label">Password</label>
             <div class="col-sm-10">
-              <input type="password" class="form-control" name='password' placeholder="Password">
+              <input type="password" class="password form-control" name='password' placeholder="Password">
             </div>
           </div>
           <div class="form-group">
@@ -40,7 +40,7 @@
           </div>
           <div class="form-group">
             <div class="col-sm-3 pull-right">
-              <button type='<button' class="btn btn-primary">Thêm vào</button>
+              <button type='<button' onclick='createUserForm.submit();' class="btn btn-primary">Thêm vào</button>
               <button type="reset" class="btn btn-default">Xóa</button>
             </div>
           </div>
