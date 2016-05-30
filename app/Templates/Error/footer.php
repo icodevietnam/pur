115e326f8defcd8a3146cc984cc00f162c11e6a6
Phan Tuan Tu <icoding.active@gmail.com>
2016-05-30 13:16:11 +0700

    --- Commit something new ---

---
+++ app/Templates/Error/footer.php
@@ -0,0 +1,5 @@
+	<div class="footer">
+	 Design by-<a href="http://icodevietnam.com">Icoding Active</a>
+	</div>
+	
+</body>
---
+++ app/Views/Error/404.php
@@ -0,0 +1,9 @@
+<div class="wrap">
+	<div class="logo">
+			<p>OOPS! - Could not Find it</p>
+			<img src="<?= URL::templateErrorPath() ?>images/404-1.png"/>
+			<div class="sub">
+			  <p><a href="<?= DIR ?>">Back </a></p>
+			</div>
+	</div>
+ </div>	
