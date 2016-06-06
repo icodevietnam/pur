3c2da6edcbdd40f2c1ed90a3fbb6082299026fac
Louis Phan <icoding.active@gmail.com>
2016-06-07 06:40:46 +0700

    ana

---
+++ app/Views/Admin/Preference.php
@@ -0,0 +1,25 @@
+<div class="row">
+  <div class="col-lg-12">
+    <div class="ibox">
+      <div class="ibox-content">
+        <div class="alert"></div>
+        <a href="<?=DIR;?>admin/~user" class="btn-link">
+          <h2>Cài đặt thông tin shop</h2>
+        </a>
+        <hr />
+        <a href="<?=DIR?>admin/~create?token=<?= Session::get('token') ?>&object=preference" data-toggle="modal" class="btn btn-sm btn-primary">Tạo mới thông tin shop</a>
+        <div class="table-responsive">
+          <table id="tblItems"
+              class="table table-bordered table-hover table-striped">
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php
+Assets::js([
+    Url::templateAdminPath().'js/page/admin-list-preference.js',
+]);
+?>
